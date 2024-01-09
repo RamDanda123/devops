e01ca31228ae2eb1e60462a419e7249680065779
ravi <ravi@vytec.com>
2024-01-09 11:44:09 +0530

    added

--- stack.sh
+++ stack.sh
@@ -1,4 +1,7 @@
-aws cloudformation create-stack --stack-name myteststack --template-body file://ec2.yml
+
+#aws cloudformation create-stack --stack-name myteststack --template-body file://ec2.yml
+
+aws cloudformation update-stack --stack-name myteststack --template-body file://ec2.yml
 
 # aws cloudformation delete-stack \
 #               --stack-name myteststack
