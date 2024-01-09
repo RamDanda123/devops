650e2e8c93b263fc80b3dbe263c59ece8277864a
ravi <ravi@vytec.com>
2024-01-09 10:02:40 +0530

    added

---
+++ ec2.sh
@@ -0,0 +1,1 @@
+aws ec2 run-instances --image-id ami-0005e0cfe09cc9050 --count 1 --instance-type t2.micro --key-name virginia --security-group-ids sg-01677a44e63d897be --subnet-id subnet-09377a43c76e65fdb
