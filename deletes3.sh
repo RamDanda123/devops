e3da7fb8e762a136e41b4bcc7af43cb498dd1e68
ravi <ravi@vytec.com>
2024-01-09 10:29:03 +0530

    added

--- deletes3.sh
+++ deletes3.sh
@@ -1,1 +1,6 @@
-for /f tokens=3 %%i in ('aws s3 ls') do aws s3 rb s3://%%i --force
+#for /f tokens=3 %%i in ('aws s3 ls') do aws s3 rb s3://%%i --force
+
+for bucket in $(aws s3 ls | awk '{print $3}'); 
+do  
+  aws s3 rb "s3://${bucket}" --force ; 
+done
