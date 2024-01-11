a4177db00cc739389e039bce1e45f2739ece3c52
ravi <ravi@vytec.com>
2024-01-11 09:39:53 +0530

    added

---
+++ create.sh
@@ -0,0 +1,7 @@
+
+aws cloudformation create-stack --stack-name myteststack --template-body file://ec2.yml
+
+#aws cloudformation update-stack --stack-name myteststack --template-body file://ec2.yml --capabilities CAPABILITY_AUTO_EXPAND
+
+# aws cloudformation delete-stack \
+#               --stack-name myteststack
